9394837fa0f71374d994d7e46e18d758162a21b3
Samarpan Harit <haritsamarpan7@gmail.com>
2020-03-04 23:08:55 +0530

    Removed exposed API key

--- getWeather.js
+++ getWeather.js
@@ -1,5 +1,7 @@
+require('dotenv').config()
 const axios = require('axios');
-const APIKEY = '852856acadefbc0c221096b940aab3b6'
+const APIKEY = process.env.OPENWEATHER_API_KEY
+// console.log(APIKEY)
 const getWeather = location => {
     return new Promise((resolve, reject, error) => {
         let result = axios.get(`http://api.openweathermap.org/data/2.5/weather?q=${location}&units=imperial&appid=${APIKEY}`).then(response => {
